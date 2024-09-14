879f3393f8f792ccb1ce5db13ef1e6b9cda836b1
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-09-14 09:06:34 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+//*****************************************************************************
+//** 2419. Longest Subarray With Maximum Bitwise AND   leetcode              **
+//*****************************************************************************
+
+int longestSubarray(int* nums, int numsSize)
+{
+    int mx = INT_MIN;  // Initialize to a very small value
+    int ans = 0, cnt = 0;
+
+    // Find the maximum element in the array
+    for (int i = 0; i < numsSize; i++)
+    {
+        if (nums[i] > mx)
+        {
+            mx = nums[i];
+        }
+    }
+
+    // Now find the length of the longest subarray with the maximum element
+    for (int i = 0; i < numsSize; i++)
+    {
+        if (nums[i] == mx)
+        {
+            ++cnt;
+            if (cnt > ans)
+            {
+                ans = cnt;
+            }
+        }
+        else
+        {
+            cnt = 0;
+        }
+    }
+
+    return ans;
+}
